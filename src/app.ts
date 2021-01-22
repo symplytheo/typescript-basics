7864bb50828d9c59106df7aedab8e9ff3d83bbf5
Theo Iyonor <symplytheo@gmail.com>
2021-01-22 08:47:28 +0100

    Initialize Finance Logger app with typescript

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+
+// form
+const form = document.querySelector('.new-item-form') as HTMLFormElement
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault()
+
+  let doc: HasFormatter
+
+  if(type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+  }
+
+  const ul = document.querySelector('ul')!
+  const list = new ListTemplate(ul)
+  list.render(doc, type.value, 'end')
+})
